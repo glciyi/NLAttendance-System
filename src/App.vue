b46b4bfe79346f51642c2f6de68e71ce9e5a97ec
郭霖 <glciyi@outlook.com>
2018-11-30 15:21:27 +0800

    gl 修改 app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <button @click = "dianji">{{ btn }}</button>
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      btn : '按钮'
+    }
+  },
+  methods : {
+    dianji () {
+      alert(1);
+    }
+  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
